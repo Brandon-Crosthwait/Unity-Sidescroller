4262df9cf102711bb1347ba382f1a8d0b1d019b0
Cprescott1 <89805294+Cprescott1@users.noreply.github.com>
2022-01-20 16:55:35 -0700

    Added additional functionality to the Main Menu

--- 2d_platformer/Assets/Scripts/MainMenu.cs
+++ 2d_platformer/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    //private float masterVolume = 0;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("LOADED GAME");
     }
 
     public void QuitGame()
     {
         Debug.Log("QUIT!");
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void ApplyVolume()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+    }
+
+    public void LoadVolume()
+    {
+        //masterVolume = PlayerPrefs.GetFloat("masterVolume");
+        volumeTextValue.text = PlayerPrefs.GetFloat("masterVolume").ToString("F1");
+        volumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
     }
 }
