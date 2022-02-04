2421e724afc9d6cd8d1af9d21a78d475db21e609
daysondarby <60206667+daysondarby@users.noreply.github.com>
2022-02-03 17:06:50 -0700

    Refactoring/Comments
    Refactoring/Comments

--- 2d_platformer/Assets/SpriteLevelSelectorMovement.cs
+++ 2d_platformer/Assets/SpriteLevelSelectorMovement.cs
@@ -6,52 +6,56 @@ using UnityEngine.SceneManagement;
 
 public class SpriteLevelSelectorMovement : MonoBehaviour
 {
-
     // Animator assigned to the player
     private Animator animator;
 
+    //booleans
     private bool isTriggered = false;
     private bool isStartPosReached = false;
     private bool isMoving = false;
 
+    //floats
     private float charMoveSpeed = 3f; //set sprite movespeed
+    float step; //how far to move sprite in each frame
 
-    string previousLevel;
+    //strings
+    string previousLevel; //for loading previous scene
 
-    float step;
-
+    //GameObjects
     public GameObject levelSelectStartPosition; //starting checkpoint
     public GameObject spriteLevelSelector; //character model
     private GameObject currentPosition; //current character position
-    private Vector3 desiredPosition; //where you want the character to go position
 
+    //Vectors
+    private Vector3 desiredPosition; //where you want the character to go position
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>(); //initialize animator
         animator.SetFloat("Speed", charMoveSpeed); //set animator speed
-        //set all booleans to false
+
+        //reset starting variables
         isStartPosReached = false;
         isTriggered = false;
         isMoving = false;
+        desiredPosition = new Vector3(0f, 0f, 0f);
 
-        previousLevel = PlayerPrefs.GetString("PreviousLevel");
+        previousLevel = PlayerPrefs.GetString("PreviousLevel"); //Retrieved level last played or on
 
+        //If a level has previously been played, find the correct trigger to start sprite on
+        //i.e. if last level was level 1, start sprite on Gameobject that contains variable for level 1
         foreach(GameObject GO in Resources.FindObjectsOfTypeAll(typeof(GameObject)))
         {
             if(GO.tag == "Level Selection Box")
             {
                 if(GO.name == previousLevel)
                 {
-                    Debug.Log("Success" + previousLevel + "is a match to" + GO.tag);
                     spriteLevelSelector.transform.position = GO.transform.position;
                     isStartPosReached = true;
                 }
             }
         }
-
-        desiredPosition = new Vector3(0f, 0f, 0f);
     }
 
     // Update is called once per frame
@@ -63,11 +67,13 @@ public class SpriteLevelSelectorMovement : MonoBehaviour
             SceneManager.LoadScene(Build.sceneOrder.MainMenu.ToString());
         }
         step = animator.GetFloat("Speed") * Time.deltaTime;
+
         //Move Character to Starting Position
         if (isTriggered == false && isStartPosReached == false)
         {
             spriteLevelSelector.transform.position = Vector3.MoveTowards(spriteLevelSelector.transform.position, levelSelectStartPosition.transform.position, step);
         }
+
         //Once Starting Position is Reached, Go to Idle
         if (isTriggered == true && isStartPosReached == false)
         {
@@ -93,52 +99,32 @@ public class SpriteLevelSelectorMovement : MonoBehaviour
 
         //Determine if player can go any of the following directions and move character if that direction is applicable
         /******************************************************************************************/
-        if(isTriggered == true && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
+        if(isTriggered == true) //if sprite is standing on select point
         {
-            if(currentPosition.transform.Find("W") != null)
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
-                animator.SetFloat("Speed", charMoveSpeed);
-                isTriggered = false;
-                isMoving = true;
-                desiredPosition = new Vector3(spriteLevelSelector.transform.position.x, spriteLevelSelector.transform.position.y + 100f, spriteLevelSelector.transform.position.z);
+                if (currentPosition.transform.Find("W") != null) MoveSpriteToNext(0f, 100f); //move sprite up
+            }
+
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if (currentPosition.transform.Find("A") != null) MoveSpriteToNext(-100f, 0f); //move sprite left
+            }
+
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (currentPosition.transform.Find("S") != null) MoveSpriteToNext(0f, -100f); //move sprite down
+            }
+
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (currentPosition.transform.Find("D") != null) MoveSpriteToNext(100f, 0f); //move sprite right
             }
         }
         
-
-        if (isTriggered == true && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)))
-        {
-            if (currentPosition.transform.Find("A") != null)
-            {
-                animator.SetFloat("Speed", charMoveSpeed);
-                isTriggered = false;
-                isMoving = true;
-                desiredPosition = new Vector3(spriteLevelSelector.transform.position.x -100f, spriteLevelSelector.transform.position.y, spriteLevelSelector.transform.position.z);
-            }
-        }
-
-        if (isTriggered == true && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
-        {
-            if (currentPosition.transform.Find("S") != null)
-            {
-                animator.SetFloat("Speed", charMoveSpeed);
-                isTriggered = false;
-                isMoving = true;
-                desiredPosition = new Vector3(spriteLevelSelector.transform.position.x, spriteLevelSelector.transform.position.y -100f, spriteLevelSelector.transform.position.z);
-            }
-        }
-
-        if (isTriggered == true && (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)))
-        {
-            if (currentPosition.transform.Find("D") != null)
-            {
-                animator.SetFloat("Speed", charMoveSpeed);
-                isTriggered = false;
-                isMoving = true;
-                desiredPosition = new Vector3(spriteLevelSelector.transform.position.x + 100f, spriteLevelSelector.transform.position.y, spriteLevelSelector.transform.position.z);
-            }
-        }
         /********************************************************************************************/
 
+        //keep the sprite moving a direction until a trigger is reached
         if (isMoving == true && isTriggered == false)
         {
             spriteLevelSelector.transform.position = Vector3.MoveTowards(spriteLevelSelector.transform.position, desiredPosition, step);
@@ -147,25 +133,31 @@ public class SpriteLevelSelectorMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        animator.SetFloat("Speed", 0f);
-        isTriggered = true;
-        isMoving = false;
-        currentPosition = collision.gameObject;
-        GameObject levelSelectPoint = collision.gameObject;
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            levelSelectPoint.transform.GetChild(i).gameObject.SetActive(true);
-        }
+        animator.SetFloat("Speed", 0f); //set speed to 0
+        isTriggered = true; //set is triggered to true to allow user choice in update
+        isMoving = false; //set is moving to false to prevent sprite movement until choice
+        currentPosition = collision.gameObject; //reset sprite position
+        GameObject levelSelectPoint = collision.gameObject; //get current game object for further manipulation
+        
+        //enable on-screen text for current level info by turning on child object
+        for (int i = 0; i < transform.childCount; i++) levelSelectPoint.transform.GetChild(i).gameObject.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        isTriggered = false;
-        GameObject levelSelectPoint = collision.gameObject;
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            levelSelectPoint.transform.GetChild(i).gameObject.SetActive(false);
-        }
+        isTriggered = false; //remove old trigger condition
+        GameObject levelSelectPoint = collision.gameObject; //get current game object for further manipulation
+
+        //disable on-screen text for current level info by turning off child object
+        for (int i = 0; i < transform.childCount; i++) levelSelectPoint.transform.GetChild(i).gameObject.SetActive(false);
     }
 
+    //helper function to move sprite in desired direction
+    private void MoveSpriteToNext(float newX, float newY)
+    {
+        animator.SetFloat("Speed", charMoveSpeed);
+        isTriggered = false;
+        isMoving = true;
+        desiredPosition = new Vector3(spriteLevelSelector.transform.position.x + newX, spriteLevelSelector.transform.position.y + newY, spriteLevelSelector.transform.position.z);
+    }
 }
