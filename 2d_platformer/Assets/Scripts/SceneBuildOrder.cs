a36b7d05176949b3ca57ca3abd9b83d1482947ba
daysondarby <60206667+daysondarby@users.noreply.github.com>
2022-02-07 16:00:18 -0700

    Scene Order Update
    Scene Order Update

--- 2d_platformer/Assets/Scripts/SceneBuildOrder.cs
+++ 2d_platformer/Assets/Scripts/SceneBuildOrder.cs
@@ -9,10 +9,10 @@ public class Build : MonoBehaviour
     public enum sceneOrder
     { 
         MainMenu = 0,
-        LevelSelect = 1,
-        LevelOne = 2,
-        LevelOneBoss = 3,
-        LevelTwo = 4
+        LevelSelect = 4,
+        LevelOne = 1,
+        LevelOneBoss = 2,
+        LevelTwo = 3
     }
 
 }
