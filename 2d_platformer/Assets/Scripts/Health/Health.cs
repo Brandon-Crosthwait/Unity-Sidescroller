cecd825437ef615cde3690e872b0ea91afc2bff3
William McConnell <william.mcconnell0@gmail.com>
2022-02-08 17:15:51 -0700

    Unit Tests
    Updated project to include unit tests and updates to the Health class to facilitate those tests.

--- 2d_platformer/Assets/Scripts/Health/Health.cs
+++ 2d_platformer/Assets/Scripts/Health/Health.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-
+    private HealthHelper hHelper;
     //SerializedFiled allows to be seen in the editor
     [SerializeField] private float startingHealth;
     [SerializeField] private AudioClip hurtSound;
     [SerializeField] private AudioClip deathSound;
 
     //holds the player's current health. get set/being public is there so it can only curently be grabbed from Healthbar.cs
-    public float currentHealth {get; private set; }
-
+    public float currentHealth;
     private void Awake()
     {
+        hHelper = new HealthHelper();
         //Sets the player health to the max health
-        currentHealth = startingHealth;
+        hHelper.setStartingHealth(ref currentHealth, startingHealth);
     }
 
     public void TakeDamage(float _damage)
     {
-        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        hHelper.takeDamage(ref currentHealth, _damage, startingHealth);
 
         if (currentHealth > 0)
         {
@@ -38,11 +38,12 @@ public class Health : MonoBehaviour
     // Method used to increase the player's health when they pick up a HealthCollectable
     public void IncreaseHealth(float healthToGive)
     {
-        currentHealth += healthToGive;
-        if(currentHealth > 3)
-        {
-            currentHealth = 3;
-        }
+        hHelper.IncreaseHealth(ref currentHealth, healthToGive, startingHealth);
+    }
+
+    public void IncreaseHealth()
+    {
+        hHelper.IncreaseHealth(ref currentHealth, startingHealth);
     }
 
     // Update is called once per frame
@@ -55,4 +56,27 @@ public class Health : MonoBehaviour
             GetComponent<PlayerMovement>().GetHit();
         }  
     }*/
+}
+
+public class HealthHelper
+{
+    public void setStartingHealth(ref float health, float startingHealth)
+    {
+        health = startingHealth;
+    }
+
+    public void IncreaseHealth(ref float health, float maxHealth)
+    {
+        health = Mathf.Clamp(health + 1, 0, maxHealth);
+    }
+
+    public void IncreaseHealth(ref float health, float amount, float maxHealth)
+    {
+        health = Mathf.Clamp(health + amount, 0, maxHealth);
+    }
+
+    public void takeDamage(ref float health, float damage, float maxHealth)
+    {
+        health = Mathf.Clamp(health - damage, 0, maxHealth);
+    }
 }
