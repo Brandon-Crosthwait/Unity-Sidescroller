2d667e62200783ff5f8ed4a04e48c14e98b436cf
brandolph <42746790+brandolph74@users.noreply.github.com>
2022-02-19 17:13:23 -0700

    More stuff for the next boss

--- 2d_platformer/Assets/FlipScript.cs
+++ 2d_platformer/Assets/FlipScript.cs
@@ -12,6 +12,7 @@ public class FlipScript : MonoBehaviour
     void Start()
     { 
         renderer = transform.GetComponent<SpriteRenderer>();
+        renderer.flipX = false;
     }
 
     // Update is called once per frame
@@ -23,7 +24,7 @@ public class FlipScript : MonoBehaviour
            //transform.localScale = new Vector3(-17, 17, 1);  //change localScale to have him face right
 
         }
-        else if (aiPath.desiredVelocity.x <= 0.01f)  //if his speed is moving left
+        else if (aiPath.desiredVelocity.x <= -0.01f)  //if his speed is moving left
         {
             renderer.flipX = true;
             //transform.localScale = new Vector3(17f, 17f, 1f);  //change localScale to have him face Lect
