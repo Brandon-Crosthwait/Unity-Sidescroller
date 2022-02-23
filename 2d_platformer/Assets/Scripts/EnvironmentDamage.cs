4d0a82ee79d4d69dbbb1cb71e61abf5d370a054c
Cprescott1 <89805294+Cprescott1@users.noreply.github.com>
2022-02-22 18:11:04 -0700

    Revert "Added Unit Test for EnvironmentDamage Script"
    This reverts commit 3a2ba847ac6abcc0300c763fbb3e95678b96d15c.

--- 2d_platformer/Assets/Scripts/EnvironmentDamage.cs
+++ 2d_platformer/Assets/Scripts/EnvironmentDamage.cs
@@ -1,54 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using System;
 
 public class EnvironmentDamage : MonoBehaviour
 {
     [SerializeField] private float damage;
     [SerializeField] private Health playerHealth;
 
-    [SerializeField] private EnvironmentDamageHelper edHelper ;
-
-    private void Awake() 
-    {
-        //edHelper = new EnvironmentDamageHelper();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        edHelper.playerTakesDamage(damage, collision);
-    }
-}
-
-[System.Serializable]
-public class EnvironmentDamageHelper
-{
-    [SerializeField] FloatEvent playerTakeDamage;
-    [SerializeField] UnityEvent playerGetsHit;
-
-    public bool wereEventsTriggered {get; set;}
-    public void playerTakesDamage(float damage, Collider2D player)
-    {
-        
-        wereEventsTriggered = false;
-        if(player.tag == "Player" && player.GetComponent<Health>().currentHealth != 0)
+        if(collision.tag == "Player" && playerHealth.currentHealth != 0)
         {
-            try
-            {
-                playerTakeDamage.Invoke(damage);
-                playerGetsHit.Invoke();
-                
-            }
-            catch (NullReferenceException e)
-            {
-                Debug.Log(e);
-            }
-            // player.GetComponent<Health>().TakeDamage(damage);
-            // player.GetComponent<PlayerMovement>().GetHit();
-            // ScoreScript.scoreValue -= 5;
-            wereEventsTriggered = true;
+            collision.GetComponent<Health>().TakeDamage(damage);
+            collision.GetComponent<PlayerMovement>().GetHit();
         }
     }
 }
