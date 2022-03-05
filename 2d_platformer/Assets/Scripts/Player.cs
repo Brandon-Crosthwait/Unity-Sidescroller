30d7bccf1bb49bf673356a37acfb87dfa253fc8c
Bandhaus <crosthwaitbrandon@gmail.com>
2022-03-05 13:29:48 -0700

    Player Script
    Player Script

---
+++ 2d_platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Player()
+{
+    private static int id = 0;
+    public string name;
+    public float score;
+    public float health;
+    public int level;
+    public int checkpoint;
+
+    public Player(string name, float score, float health, int level, int checkpoint)
+    {
+        id += 1;
+        this.name = name;
+        this.score = score;
+        this.health = health;
+        this.level = level;
+        this.checkpoint = checkpoint;
+    }
+}
