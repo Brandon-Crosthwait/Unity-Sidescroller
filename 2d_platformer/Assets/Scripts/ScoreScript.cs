3238da1e91ebeb22d52ddbe82e30acc4d113073d
Cprescott1 <89805294+Cprescott1@users.noreply.github.com>
2022-04-20 16:26:49 -0600

    Made some score script adjustments

--- 2d_platformer/Assets/Scripts/ScoreScript.cs
+++ 2d_platformer/Assets/Scripts/ScoreScript.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
-    public static int scoreValue = 0;
+    public static int scoreValue;
     Text score;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        scoreValue = 0;
         score = GetComponent<Text> ();
     }
 
